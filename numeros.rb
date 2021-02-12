99d60b3deb8eb8e2f4dc731f6c3347aec518a660
Erick Gabriel dos Santos Alves <erickgabrielalves@hotmail.com>
2021-02-12 16:43:16 -0300

    Trabalhando com Números

---
+++ numeros.rb
@@ -0,0 +1,21 @@
+# Imprimindo números
+# puts 42
+# puts 42.42
+
+# O Ruby igonora o "_"
+# puts 1_500
+
+# Tipos de números
+# puts 42.class
+# puts 42.42.class
+
+# Soma
+# soma = 5 + 5.5
+# puts "Soma #{soma}"
+# Chamando o método '+'
+# p 5.send("+", 3)
+
+# Verificando se o número é par
+p 10.even?
+# Verificando se o número é impar
+p 11.odd?
