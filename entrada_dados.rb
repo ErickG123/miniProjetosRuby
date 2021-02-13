e80a4befbebaaba0cd734e72941c38c88517fc6d
Erick Gabriel dos Santos Alves <erickgabrielalves@hotmail.com>
2021-02-12 22:19:14 -0300

    Trabalhando com Entrada de Dados

---
+++ entrada_dados.rb
@@ -0,0 +1,8 @@
+# 'gets' para o usuário digitar o valor
+# nome = gets
+# .chomp() para tirar o caractere '\n' do final do input
+# p nome.chomp()
+# Ou
+puts "Digite o seu nome: "
+nome = gets.chomp()
+p "Seu nome é #{nome}"
